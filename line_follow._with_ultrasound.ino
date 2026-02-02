667253afd6d9192b82e3bd4bad6d535bbfb9fedf
joshrco-25 <jorkingmyshii69@gmail.com>
2026-02-02 11:26:14 +0800

    Update and rename line_follow.ino to line_follow._with_ultrasound.ino

--- line_follow._with_ultrasound.ino
+++ line_follow._with_ultrasound.ino
@@ -1,5 +1,3 @@
-/*The program show how to program the motor to turn left*/
-
 #define motor_board_input_pin_IN2 6     //RIGHT BLACK
 #define motor_board_input_pin_IN1 9     //RIGHT RED
 #define motor_board_input_pin_IN4 3     //LEFT RED
@@ -8,22 +6,37 @@
 #define leftIR A0
 #define rightIR A1
 
+#define speakerPin 4
+#define TrigPin1 11   // U/S1 Trig connected to pin 11
+#define EchoPin1 10   // U/S1 Echo connected to pin 10
+
 int left_val = 0;
 int right_val = 0;
+
+int dist;
 
 void setup() 
 {
   Serial.begin(9600);
 
+  //motor
   pinMode(motor_board_input_pin_IN2, OUTPUT); 
   pinMode(motor_board_input_pin_IN1, OUTPUT); 
   pinMode(motor_board_input_pin_IN4, OUTPUT); 
   pinMode(motor_board_input_pin_IN3, OUTPUT);
+
+  //ultrasound sensor
+  pinMode(TrigPin1, OUTPUT);  //Arduino's output, ranger's input
+  pinMode(EchoPin1, INPUT);   //Arduino's input, ranger's output
+
+  //buzzer
+  pinMode(speakerPin, OUTPUT);
 }
 
 void loop() {
   pathCheck();
   movement();
+  us();
 }
 
 void pathCheck(){
@@ -57,26 +70,79 @@ void movement()
     analogWrite(motor_board_input_pin_IN1, 255);
     
     digitalWrite(motor_board_input_pin_IN3, HIGH);
-    analogWrite(motor_board_input_pin_IN4, 120);
+    analogWrite(motor_board_input_pin_IN4, 140);
   } 
 
   //right
   if(left_val > 800 && right_val < 40){
     digitalWrite(motor_board_input_pin_IN2, HIGH);
-    analogWrite(motor_board_input_pin_IN1, 120);
+    analogWrite(motor_board_input_pin_IN1, 140);
     
     digitalWrite(motor_board_input_pin_IN3, HIGH);
     analogWrite(motor_board_input_pin_IN4, 255);
   } 
 
-  //stop
+  //backwards
   if(left_val > 800 && right_val > 800){
+    digitalWrite(motor_board_input_pin_IN2, LOW);
+    analogWrite(motor_board_input_pin_IN1, 150);
+    digitalWrite(motor_board_input_pin_IN3, LOW);
+    analogWrite(motor_board_input_pin_IN4, 70);
+
+    delay(200);
+  } 
+}
+
+//ultrasound sensing code
+void us(){
+  long pulseDuration; //variable needed by the ultrasound sensor code
+  int Distance;       // Ultrasound distance in cm
+
+  // this main code runs repeatedly:
+  // 1. Produce a 10us (micro-second) HIGH pulse in Trig to trigger the sensor...
+  digitalWrite(TrigPin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TrigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TrigPin1, LOW);
+
+  // 2. Use pulseIn() function to measure the duration of the HIGH pulse in Echo 
+  // every 58 us is an obstacle distance of 1 cm
+
+  pulseDuration = pulseIn(EchoPin1, HIGH);
+  Distance = pulseDuration / 58;  //  Convert to cm
+
+  dist = Distance;
+
+  // 3. display the obstacle distance in serial monitor 
+  Serial.print("Distance = ");
+  Serial.print(Distance);
+  Serial.println(" cm");
+  if(Distance < 50){
+    Serial.println("Too close");
+  }
+  delay(5);
+
+  detectObs();
+}
+
+
+//detects obstacles
+void detectObs(){
+  if(dist < 10){
     digitalWrite(motor_board_input_pin_IN2, HIGH);
     analogWrite(motor_board_input_pin_IN1, 255);
-    
     digitalWrite(motor_board_input_pin_IN3, HIGH);
     analogWrite(motor_board_input_pin_IN4, 255);
-  } 
+
+    for(int freq = 500; freq <= 1000; freq += 10){
+      tone(speakerPin, freq);
+      delay(5);
+    }
+  }
+  else{
+    noTone(speakerPin);
+  }
 }
 
 
